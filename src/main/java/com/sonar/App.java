c71140269f8536e077b6e088eddde187dc1f0832
saiurakrishna <sandeep_devops21_gmail_com@sonar.us-east1-b.c.spring-carving-261907.internal>
2020-02-15 17:18:37 +0000

    origin

---
+++ src/main/java/com/sonar/App.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
